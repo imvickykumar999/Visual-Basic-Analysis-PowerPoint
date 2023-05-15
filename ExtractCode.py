7e0cc7e8f4f577e771e65118b4ff6cef9c555428
imvickykumar999 <imvickykumar999@gmail.com>
2023-05-15 22:34:02 +0530

    pip install python-pptx
    https://python-pptx.readthedocs.io/en/latest/user/quickstart.html

--- ExtractCode.py
+++ ExtractCode.py
@@ -1,15 +1,19 @@
 
-import aspose.slides as slides
+# https://python-pptx.readthedocs.io/en/latest/user/quickstart.html
+# pip install python-pptx
+from pptx import Presentation
 
-# Load a presentation
-with slides.Presentation("PPTs/presentation.ppt") as presentation:
-    
-    # Check if presentation contains VBA Project
-    if presentation.vba_project is not None:
-        
-        # Print each module
-        for module in presentation.vba_project.modules:
-            print(module.name)
-            print(module.source_code)
-    else:
-        print('Presentation does NOT contains VBA Project')
+path_to_presentation = 'PPTs/PlayFair Ciphertext Encryption.pptx'
+prs = Presentation(path_to_presentation)
+text_runs = []
+
+for slide in prs.slides:
+    for shape in slide.shapes:
+        print(shape.textframe.paragraphs)
+    print()
+        # if not shape.has_textframe:
+        #     continue
+        # for paragraph in shape.textframe.paragraphs:
+        #     for run in paragraph.runs:
+        #         text_runs.append(run.text)
+
